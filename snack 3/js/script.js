81cba849bdd4018f5fbfbf31d00b722635231bf3
RiccardoNestola <riccardinux@gmail.com>
2022-11-28 17:43:51 +0100

    snack 3

---
+++ snack 3/js/script.js
@@ -0,0 +1,69 @@
+
+// Crea due array che hanno un numero di elementi diversi. 
+//Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+
+
+const shoppingCart = [
+    "pane",
+    "pasta",
+    "latte",
+    "uova",
+    
+];
+
+const shoppingList = [
+    "caffe",
+    "zucchero",
+];
+
+let products = 0;
+
+while ((shoppingList.length != shoppingCart.length)){
+    let product = prompt('inserisci dei prodotti');
+    
+    if(shoppingList.length < shoppingCart.length){
+        shoppingList.push(product);
+    } else if(shoppingList.length > shoppingCart.length){
+        longList.push(product);
+    }
+    product += products;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
